da4ad37a0dad380e1938a5cafeb72d3f61819380
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2022-11-29 22:08:42 -0600

    Update search_bar.js

--- static/src/js/search_bar.js
+++ static/src/js/search_bar.js
@@ -1,66 +1,23 @@
-odoo.define('sucasa.ProductScreenClick', function(require) {
-    "use strict";
-
-    const ProductScreenClick = require('point_of_sale.ProductScreen');
-    const Registries = require('point_of_sale.Registries');
-    var models = require('point_of_sale.models');
-
-    models.load_fields('pos.config', 'device_id');
-
-    const DevProductScreenClick = ProductScreenClick => class extends ProductScreenClick {
-        //@Override
-
-        constructor() {
-            super(...arguments);
-
-        }
-
-        async _getAddProductOptions(product, base_code) {
-            console.log('_getAddProductOptions')
-            console.log(product)
-            console.log(base_code)
-            var super_product_options = await super._getAddProductOptions(product,base_code)
-            if (product.to_weight){
-                var device_id = this.env.pos.config.device_id
-                console.log(device_id)
-                var weight = await this._getSucasaWeight(device_id)
-                super_product_options.quantity = weight
-                return super_product_options
-            }else{
-                return super_product_options
-            }
-        }
+odoo.define('sucasa.search_bar', function (require) {
+  'use strict';
 
 
-        async _getSucasaWeight(device_id){
-            var self = this;
-                return await this.rpc({
-                        model: 'pos.order',
-                        method: 'get_sucasa_product_weight',
-                        args: [[], [device_id]],
-                    }, {
-                        timeout: 10000,
-                        shadow: false,
-                    }).catch(function(error){
-                        //self.set_synch(self.get(
-                            console.log('err1')
-                            return error;
-                    }).then( function(response) {
-                            console.log('th response')
-                            console.log(response)
-                            return response;
+  const SearchBar = require('web.SearchBar');
+  const utils = require('web.utils');
+  const { patch } = require('web.utils');
 
 
-                    });
-
-
-        }
-
-
+  patch(SearchBar.prototype, 'sucasa.mypatch2', {
+    _onSearchInput(ev) {
+      console.log(this)
+      if (this && this.env && this.env.action && this.env.action.xml_id && this.env.action.xml_id == "sucasa.sucasa_product_template_action"){
+          setTimeout(function(){
+             window.location.reload(1);
+          }, 2000);
+      }
+      this._super(ev);
 
     }
-
-    Registries.Component.extend(ProductScreenClick, DevProductScreenClick);
-
-    return ProductScreenClick;
 });
+});
+
